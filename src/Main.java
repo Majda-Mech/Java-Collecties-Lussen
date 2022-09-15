1a51985d2ee4cd5892932548c00e91e841843f37
Ellen van Duikeren <e.vanduikeren@gmail.com>
2022-09-15 08:26:55 +0200

    Changed some minor details, added comments

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,42 @@
 //Opdrachtbeschrijving
 // Je gaat een applicatie bouwen die getallen vertaalt van numeriek (1, 2, 3, etc.) naar alfabetisch (een, twee, drie, etc.). Je gaat die getallen vertalen door gebruik te maken van een HashMap. Je zet de numerieke getallen als sleutel (key) en de alfabetische getallen als waarde (value) in de HashMap. Vervolgens vraag je de gebruiker om een input van 0 t/m 9 te geven en ga je dat "vertalen" door simpelweg de waarde uit de HashMap te vragen met de bijbehorende sleutel en dat terug te geven aan de gebruiker.
 
-
 import java.util.Scanner;
 
 public class Main {
+    //In je main methode staan twee arrays (1 numeriek en 1 alfabetisch), een boolean variabele, een Translator object en een Scanner object;
 
     public static void main(String[] args) {
-        System.out.println("Give a number (0 - 9):");
-        Scanner userInput = new Scanner(System.in);
-        int number = userInput.nextInt();
-        System.out.println("Your number is: " + number);
+        //De variabelen zijn in de constructor benoemd. Als je ze in de class hierboven benoemt, dan kan de code er niet bij, tenzij hij deze variabelen ook static maakt, net als main method.
+        int[] numeric = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        String[] alphabetic = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "zero"};
+        boolean play = true;
+        String str = "invalid input";
 
-        // je kan hier niet direct het nummer opvragen via hashMapWithNumbers.get(number); dan zegt intelliJ iets over static methods.
-        // je kan ook niet direct de translate functie aanroepen, want die is niet bekend hier in main.
-        // Dus dan moet je eerst een instance van de class Translator maken en daar kan je dan weer de translate functie over roepen.
-        Translator trans = new Translator();
-        trans.translate(number);
+        Translator translator = new Translator(numeric, alphabetic);
+        Scanner scanner = new Scanner(System.in);
+
+        while (play == true) {
+            System.out.println("Type 'x' to stop\nType 'v' to translate");
+            String input = scanner.nextLine().toLowerCase();
+
+            int number;
+            if (input.equals("x")) {
+                System.out.println("Good bye");
+                play = false;
+            } else if (input.equals("v")) {
+                System.out.println("Give a number (0 - 9):");
+                number = scanner.nextInt();
+                scanner.nextLine();
+                if (number < 10) {
+                    String result = translator.translate(number);
+                    System.out.println(result);
+                } else {
+                    System.out.println(str);
+                }
+            } else {
+                System.out.println(str);
+            }
+        }
     }
-
 }
--- src/Translator.java
+++ src/Translator.java
@@ -5,32 +5,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Translator {
-    //    final toegevoegd op aanraden van intelliJ omdat we deze hashMap niet meer mogen veranderen
-    private final Map<Integer, String> hashMapWithNumbers = new HashMap<>();
+    private Map<Integer, String> numericAlpha = new HashMap<>();
 
-
-    public Translator() {
-        hashMapWithNumbers.put(1, "one");
-        hashMapWithNumbers.put(2, "two");
-        hashMapWithNumbers.put(3, "three");
-        hashMapWithNumbers.put(4, "four");
-        hashMapWithNumbers.put(5, "five");
-        hashMapWithNumbers.put(6, "six");
-        hashMapWithNumbers.put(7, "seven");
-        hashMapWithNumbers.put(8, "eight");
-        hashMapWithNumbers.put(9, "nine");
-        hashMapWithNumbers.put(0, "zero");
+    public Translator(int[] numeric, String[] alphabetic) {
+        for (int i = 0; i < numeric.length; i++) {
+            numericAlpha.put(numeric[i], alphabetic[i]);
+        }
     }
 
-    // omdat we iets uit de hashmap willen opvragen vanuit main moeten we hier een getter toevoegen
-    public Map<Integer, String> getNumbers() {
-        return hashMapWithNumbers;
-    }
-
+    // geen getter nodig, want we gaan die via constructor opvragen
     // geen setter nodig want we gaan de hashmap niet aanpassen, alleen er iets uit op halen
 
-    public void translate(int askedNumber) {
-        System.out.println("Your number in text is: " + hashMapWithNumbers.get(askedNumber));
+    // methods
+    public String translate(Integer number) {
+        return "The translation of your number " + number + " in text is: " + numericAlpha.get(number);
     }
+}
 
-} // closing class
+
+
+
