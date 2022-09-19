ad73a23d012a2247f21af13752d6dcb318ab02a5
Ellen van Duikeren <e.vanduikeren@gmail.com>
2022-09-19 10:28:04 +0200

    Opnieuw voor FB

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-//done
+//done..
 
 //Opdrachtbeschrijving
 // Je gaat een applicatie bouwen die getallen vertaalt van numeriek (1, 2, 3, etc.) naar alfabetisch (een, twee, drie, etc.). Je gaat die getallen vertalen door gebruik te maken van een HashMap. Je zet de numerieke getallen als sleutel (key) en de alfabetische getallen als waarde (value) in de HashMap. Vervolgens vraag je de gebruiker om een input van 0 t/m 9 te geven en ga je dat "vertalen" door simpelweg de waarde uit de HashMap te vragen met de bijbehorende sleutel en dat terug te geven aan de gebruiker.
@@ -6,7 +6,7 @@
 import java.util.Scanner;
 
 public class Main {
-    //In je main methode staan twee arrays (1 numeriek en 1 alfabetisch), een boolean variabele, een Translator object en een Scanner object;
+    //In je main methode staan twee arrays (1 numeriek en 1 alfabetisch), een boolean variabele, een Translator object en een Scanner object;git
 
     public static void main(String[] args) {
         //De variabelen zijn in de constructor benoemd. Als je ze in de class hierboven benoemt, dan kan de code er niet bij, tenzij hij deze variabelen ook static maakt, net als main method.
