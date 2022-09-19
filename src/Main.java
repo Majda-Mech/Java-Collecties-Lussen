e7c8d904fb8d9576a3563e0710432b1c16b5643b
Ellen van Duikeren <e.vanduikeren@gmail.com>
2022-09-19 10:36:57 +0200

    Nogmaals voor FB

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 
 //Opdrachtbeschrijving
 // Je gaat een applicatie bouwen die getallen vertaalt van numeriek (1, 2, 3, etc.) naar alfabetisch (een, twee, drie, etc.). Je gaat die getallen vertalen door gebruik te maken van een HashMap. Je zet de numerieke getallen als sleutel (key) en de alfabetische getallen als waarde (value) in de HashMap. Vervolgens vraag je de gebruiker om een input van 0 t/m 9 te geven en ga je dat "vertalen" door simpelweg de waarde uit de HashMap te vragen met de bijbehorende sleutel en dat terug te geven aan de gebruiker.
+// done
 
 import java.util.Scanner;
 
